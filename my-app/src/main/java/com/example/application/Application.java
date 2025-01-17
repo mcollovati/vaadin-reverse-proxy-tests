0611f572b6665605963ea2666d8073dceea2505e
Marco Collovati <mcollovati@gmail.com>
2025-01-17 12:07:13 +0100

    fix: handle static resources for Vaadin URL mapping

--- my-app/src/main/java/com/example/application/Application.java
+++ my-app/src/main/java/com/example/application/Application.java
@@ -1,19 +1,30 @@
 package com.example.application;
 
-import com.vaadin.flow.component.dependency.NpmPackage;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The entry point of the Spring Boot application.
  *
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
 @Theme(value = "myapp")
@@ -23,6 +34,29 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "vaadin.url-mapping")
+    FilterRegistrationBean<?> publicImagesAliasFilter(@Value("${vaadin.url-mapping}") String urlMapping) {
+        String baseMapping = urlMapping.replaceFirst("/\\*$", "");
+        FilterRegistrationBean<OncePerRequestFilter> registrationBean = new FilterRegistrationBean<>(
+                new OncePerRequestFilter() {
+
+                    @Override
+                    protected void doFilterInternal(HttpServletRequest request,
+                                                    HttpServletResponse response,
+                                                    FilterChain filterChain)
+                            throws ServletException, IOException {
+                        request.getRequestDispatcher(request.getRequestURI()
+                                        .substring(baseMapping.length()))
+                                .forward(request, response);
+                    }
+                });
+        registrationBean.addUrlPatterns(baseMapping + "/icons/icon.png",
+                baseMapping + "/images/*");
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
     }
 
 }
